145b0918800c4e50ab67a225fc3465b6c0ca6772
sebastienbesson <sebbes06@hotmail.fr>
2021-06-10 18:44:54 +0200

    maj basket + localstorage

--- script2.js
+++ script2.js
@@ -7,39 +7,55 @@ fetch('http://localhost:3000/api/furniture/'+id)
 .then(response => {return response.json()})
 .then(data => {
   console.log(data);
+  clickValid.onclick = ()=> {
+  console.log(data);
+    const productUnit = {
+      name: data.name,
+      image: data.imageUrl,
+      price: data.price,
+      ref: data._id,
+      varnish: data.varnish,
+    }
+  let currentProduct = localStorage.getItem("productsUnit");
+  if(!currentProduct){
+    currentProduct= [];
+
+  }else{currentProduct= JSON.parse(currentProduct)};
+  currentProduct.push(productUnit);
+  localStorage.setItem("productsUnit", JSON.stringify(currentProduct));
+  
+
+  }
+  
+
+
   let container = document.getElementById('container');
   let allcolumns =  `
     <div class="product">
     <h2 class="card-title" id="nameProduct">${data.name}</h2>
-    <img src="${data.imageUrl}"></img>
+    <img id="imageProduct" src="${data.imageUrl}"></img>
     <h3 id="description">${data.description}</h3>
     <div class="rowPrice text-center">
     <h4 id="price">${data.price}€</h4>
     </div>
     <p>Référence: <div class="ref">${data._id}</div></p>
-    <p><select name="varnish" id="varnish">
+    <h3>Choississez votre vernis:</br><select name="varnish" id="varnish">
     ${data.varnish.map(varnish =>
       "<option value='"+varnish+"'>"+varnish+"</option>")}
-    </select></p>
+    </select></h3>
     </div>
+   
     `
 
 container.insertAdjacentHTML("afterbegin", allcolumns)  
 
 })
 
-const finalProduct = JSON.parse(localStorage.getItem("productUnit"));
 
-clickValid.onclick = ()=> {
-  const productUnit = {
-    name: "Coffee Table",
-    price: "59900",
-    ref: "5be9cc611c9d440000c1421e"
-  }
-localStorage.setItem("productUnit",JSON.stringify(productUnit));
-}
 
-cancelChoice.onclick = ()=> {
-  localStorage.clear();
-}
+
+
+
+
+
 
--- script3.js
+++ script3.js
@@ -1,3 +1,81 @@
+localStorage.setItem("productsUnit", JSON.parse(currentProduct));
+
+let container = document.getElementById('container');
+
+let allcolumns = productUnitJSON(productUnitJSON => {
+     
+ `    
+  <div class="col-12 col-md-6 mt-4 mt-md-0 p-md-5">
+    <div class="card text-center">
+      <div class="card-body">
+        <h2 class="card-title">${productUnitJSON.name}</h2>
+        <img src="${productUnitJSON.imageUrl}"></img>
+        <div class="rowPrice text-center">
+            <h4>${productUnitJSON.price}€</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+ `
+  
+}).join("")
+container.insertAdjacentHTML("afterbegin",allcolumns)
+
+//formulaire
+
+document.getElementById("validez").addEventListener("submit", function(e){
+
+var error;
+var lastName = document.getElementById("lastName");
+var firstName = document.getElementById("firstName");
+var email = document.getElementById("email");
+     
+if (!email.value) {
+  error = "veuillez renseigner votre e-mail";
+} else if(!emailValid(email.value)){
+  error = "adresse e-mail non valide!";
+}
+   
+if (!firstName.value) {
+  error = "veuillez renseigner votre prénom";
+} else if(!firstNameValid(firstName.value)){
+  error = "votre prénom sans accent ni apostrophe!";
+}  
+
+if (!lastName.value) {
+  error = "veuillez renseigner votre nom";
+} else if(!lastNameValid(lastName.value)){
+  error = "votre nom écrit en majuscule s'il vous plaît!";
+} 
+
+if (error) {
+  e.preventDefault();
+  document.getElementById("error").innerHTML = error;
+  return false;
+} else {
+      alert('formulaire envoyé!');
+}
+})
+
+function lastNameValid(lastName){
+  return /^[A-Z]+$/.test(lastName);
+}
+
+function firstNameValid(firstName){
+  return /^[a-aA-Z]+$/.test(firstName);
+}
+
+function emailValid(email){
+  return /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/.test(email);
+}
+
+  
+
+
+
+ 
+
+
 
 
 
